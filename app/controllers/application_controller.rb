9b3e8b4a88fd5ecb283a0651a71c49ed7aa5a9a5
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-04-08 18:30:17 +0900

    ユーザー登録、データベースへの保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auch
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
 
@@ -12,4 +13,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday, :first_name, :last_name, :first_name_kana, :last_name_kana])
+  end
+
+  
 end
