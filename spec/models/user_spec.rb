935eee7b6fefa0004cf4bde2a8491107fbf80fa6
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-04-10 19:07:41 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,6 +52,12 @@ RSpec.describe User, type: :model do
         another_user.valid?
         expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
+      it 'emailに@が含まれなければ登録できない' do
+        @user.email = 'aaaaaaaaa'
+        @user.valid?
+        binding.pry
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
