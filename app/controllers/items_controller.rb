30caa962a07635198ac8e02694e247aeec629bcd
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-04-12 15:03:59 +0900

    itemモデルとcategoryモデルにアソシエーションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,12 @@ class ItemsController < ApplicationController
 
 
   def index
+    
   end
 
-  #def new
-  #end
+  def new
+    @items = Items.order("created_at DESC")
+  end
 
   #def create  
   #end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
   belongs_to :user
   has_one_attched :purchase
   has_one_attached :image
+
+  validates :category_id, numericality: { other_than: 1 }
 end
