be1ba650c5c426601cdcc506baeaedd30fac6b2f
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-04-12 15:45:17 +0900

    itemモデルにアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,20 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :area
+  belongs_to :shipping_time
 
   belongs_to :user
   has_one_attched :purchase
   has_one_attached :image
 
+  validates :price, presence: true
   validates :category_id, numericality: { other_than: 1 }
   validates :status_id, numericality: { other_than: 1 }
+  validates :shipping_cost_id, numericality: { other_than: 1 }
+  validates :area_id, numericality: { other_than: 1 }
+  validates :shipping_time_id, numericality: { other_than: 1 }
+
 end
