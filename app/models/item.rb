03c9863d72fbad0f7834533f2af7c427890a42b4
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-04-13 18:05:39 +0900

    バリデーションのメッセージの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,16 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: {with: /\A[0-9]+\z/}
-  validates :category_id, presence: true, numericality: { other_than: 1 }
-  validates :status_id, presence: true, numericality: { other_than: 1 }
-  validates :shipping_cost_id, presence: true, numericality: { other_than: 1 }
-  validates :area_id, presence: true, numericality: { other_than: 1 }
-  validates :shipping_time_id, presence: true, numericality: { other_than: 1 }
+  validates :image, presence: true
+  validates :items_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/, message: 'Half-width number'}, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range'}
+  validates :category_id, presence: true, numericality: { other_than: 1, message: 'Select' }
+  validates :status_id, presence: true, numericality: { other_than: 1, message: 'Select' }
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 1, message: 'Select' }
+  validates :area_id, presence: true, numericality: { other_than: 1, message: 'Select' }
+  validates :shipping_time_id, presence: true, numericality: { other_than: 1, message: 'Select' }
+
+  
 
 end
