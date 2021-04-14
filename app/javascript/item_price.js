265066c29378e2ea40650201a2b70e803587dddf
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-04-14 19:24:42 +0900

    itemモデルのテストコードの記述

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,2 +1,15 @@
-const priceInput = document.getElementById(item-price);
-console.log(priceInput);
+window.addEventListener('load', function(){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value
+    const tax = Math.floor(inputValue * 0.1)
+    const profit = inputValue - tax
+    const addTaxDom = document.getElementById("add-tax-price")
+    addTaxDom.innerHTML = tax
+    const addProfit = document.getElementById("profit")
+    addProfit.innerHTML = profit
+
+    //const addTaxDom = document.getElementById("profit");
+    //addTaxDom.innerHTML = "入力した金額をもとに販売手数料を計算する処理"
+  })
+})
