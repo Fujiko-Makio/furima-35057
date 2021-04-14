23cd0b72843289a131be611e61e401c6fcd0c459
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-04-14 21:22:14 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,16 +9,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :image, presence: true
-  validates :items_name, presence: true
-  validates :description, presence: true
-  validates :price, presence: true, format: {with: /\A[0-9]+\z/, message: 'Half-width number'}, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range'}
-  validates :category_id, presence: true, numericality: { other_than: 1, message: 'Select' }
-  validates :status_id, presence: true, numericality: { other_than: 1, message: 'Select' }
-  validates :shipping_cost_id, presence: true, numericality: { other_than: 1, message: 'Select' }
-  validates :area_id, presence: true, numericality: { other_than: 1, message: 'Select' }
-  validates :shipping_time_id, presence: true, numericality: { other_than: 1, message: 'Select' }
+  with_options presence: true do
+    validates :image
+    validates :items_name
+    validates :description
+  end
 
-  
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/, message: 'Half-width number'}, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range'}
+
+  with_options presence: true, numericality: { other_than: 1, message: 'Select' } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_cost_id
+    validates :area_id
+    validates :shipping_time_id
+  end
 
 end
