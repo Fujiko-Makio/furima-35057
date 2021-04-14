bb1dca83b3ee4dff1a5773b73ebcc2a60c618d3e
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-04-14 21:15:21 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :items
-  has_many :purchases
+  
+  #has_many :items
+  #has_many :purchases
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
