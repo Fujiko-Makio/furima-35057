3d1d1b14a0f4f7e702eb36af53999e6bdd50c577
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-04-17 20:14:40 +0900

    PuchaseShippingモデルにバリデーションの記述

--- app/models/PurchaseShipping.rb
+++ app/models/PurchaseShipping.rb
@@ -2,7 +2,15 @@ Class PurchaseShipping
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :area_id, :city, :address, :building_name, :phone_number
 
-  #バリデーションの記述
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :area_id, numericality: { other_than: 1 }
+    validates :city
+    validates :address
+    validates :phone_number, format: { /\A\d{11}\z/ }
+  end
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
