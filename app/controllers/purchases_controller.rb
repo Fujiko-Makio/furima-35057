6d033ca4f3c2f82be49c7b5b18aa85d759348e23
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-04-18 21:56:45 +0900

    indexアクションへのレンダー処理、エラー文の表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,25 +2,26 @@ class PurchasesController < ApplicationController
 
   def index
     @purchase = Item.find(params[:item_id])
+    @purchase_shipping = PurchaseShipping.new
   end
 
-  def new
-  end
 
   def create
-    @purchase_shipping = PurchaseShippimgs.new(purchase_params)
+    #binding.pry
+    @purchase_shipping = PurchaseShipping.new(purchase_params)
     if @purchase_shipping.valid?
       @purchase_shipping.save
-      return redirect_to root_path
+      redirect_to root_path
     else
-      render :new
+      @purchase = Item.find(params[:item_id])
+      render :index
     end
   end
 
   private
 
   def purchase_params
-    params.require(:purchase_shippings).permit(:item_id, :post_code, :area_id, :city, :building_name, :phone_number)
+    params.require(:purchase_shipping).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id)
   end
 
 end
--- app/models/PurchaseShipping.rb
+++ app/models/PurchaseShipping.rb
@@ -1,4 +1,4 @@
-Class PurchaseShipping
+class PurchaseShipping
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :area_id, :city, :address, :building_name, :phone_number
 
@@ -9,11 +9,11 @@ Class PurchaseShipping
     validates :area_id, numericality: { other_than: 1 }
     validates :city
     validates :address
-    validates :phone_number, format: { /\A\d{11}\z/ }
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
   end
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Shipping.create(post_code :post_code, area_id :area_id, city :city, address :address, building_name :building_name, phone_number :phone_number)
+    Shipping.create(post_code: post_code, area_id: area_id, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
